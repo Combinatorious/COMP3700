9e78223fe7b7dd4a9965c4501b2e778bae3af4b7
mcCarbonationalism <cai0004@auburn.edu>
2019-09-24 12:44:10 -0500

    activity10 database functional

--- activities/activity10/src/main/java/AddProductController.java
+++ activities/activity10/src/main/java/AddProductController.java
@@ -12,11 +12,32 @@ public class AddProductController implements ActionListener{
         myView = view;
         myView.saveButton.addActionListener(this);
         myView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         JOptionPane.showMessageDialog(myView, "I'll make sure to save that for you.");
+
+        try {
+            ProductModel inputProduct = new ProductModel(
+                    Integer.parseInt(myView.barcodeField.getText()),
+                    myView.nameField.getText(),
+                    Double.parseDouble(myView.priceField.getText()),
+                    Double.parseDouble(myView.quantityField.getText()),
+                    myView.supplierField.getText()
+            );
+
+
+            SQLiteWrapper dataAccess = new SQLiteWrapper();
+            dataAccess.connect();
+            dataAccess.saveProduct(inputProduct);
+        }
+        catch (Exception err) {
+            err.printStackTrace();
+        }
+
         myView.nameField.setText("");
         myView.barcodeField.setText("");
         myView.priceField.setText("");
